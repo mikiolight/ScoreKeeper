2ef16dd8610675b86532a0e98e9b66fd2a019141
mikio <43379090+mikiolight@users.noreply.github.com>
2025-12-02 00:14:06 +0900

    Section 1: Create an app with a list of players

--- ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var players : [String] = ["Elisha", "Andre", "Jasmine"]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+			ForEach(0..<players.count, id: \.description) {index in
+				TextField("Name", text: $players[index])
+			}
+
+			Button("Add Player", systemImage: "plus") {
+				players.append("")
+			}
         }
         .padding()
     }
