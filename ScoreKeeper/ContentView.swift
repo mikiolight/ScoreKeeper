bdcf7aea8b1f78569507643d4193a67e18ac71a2
mikio <43379090+mikiolight@users.noreply.github.com>
2025-12-02 12:40:49 +0900

    Section 2: Add scores

--- ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ContentView.swift
@@ -8,17 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-	@State private var players : [String] = ["Elisha", "Andre", "Jasmine"]
+	@State private var players: [String] = ["Elisha", "Andre", "Jasmine"]
+	@State private var scores: [Int] = [0,0,0]
 
     var body: some View {
         VStack {
 			ForEach(0..<players.count, id: \.description) {index in
 				TextField("Name", text: $players[index])
+				Stepper("\(scores[index])", value: $scores[index])
 			}
 
 			Button("Add Player", systemImage: "plus") {
 				players.append("")
+				scores.append(0)
 			}
+
+			Spacer()
         }
         .padding()
     }
