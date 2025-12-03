8ae1b2147a71332df5e68e4dc2b10f41d3386be4
mikio <43379090+mikiolight@users.noreply.github.com>
2025-12-03 22:42:31 +0900

    Section 3: Create your own player

---
+++ ScoreKeeper/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  ScoreKeeper
+//
+//  Created by Mikihisa Saito on 2025/12/02.
+//
+
+import Foundation
+
+struct Player : Identifiable {
+	let id = UUID()
+
+	var name: String
+	var score: Int
+}
