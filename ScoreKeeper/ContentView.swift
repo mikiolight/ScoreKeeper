bbcea7a0e422795a7ae74957f42ac40481e1841d
mikio <43379090+mikiolight@users.noreply.github.com>
2025-12-05 12:51:11 +0900

    Section 1: Create the scoreboard

--- ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ContentView.swift
@@ -8,12 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-	@State private var players: [Player] =
-	[
-		Player(name: "Elisha", score: 0),
-		Player(name: "Andre", score: 0),
-		Player(name: "Jasmine", score: 0)
-	]
+	@State private var scoreboard = Scoreboard()
 
     var body: some View {
 		VStack(alignment: .leading) {
@@ -30,7 +25,7 @@ struct ContentView: View {
 				}
 				.font(.headline)
 
-				ForEach($players) { $player in
+				ForEach($scoreboard.players) { $player in
 					GridRow{
 						TextField("Name", text: $player.name)
 						Text("\(player.score)")
@@ -42,7 +37,7 @@ struct ContentView: View {
 			.padding(.vertical)
 
 			Button("Add Player", systemImage: "plus") {
-				players.append(Player(name: "", score: 0))
+				scoreboard.players.append(Player(name: "", score: 0))
 			}
 
 			Spacer()
---
+++ ScoreKeeper/Scoreboard.swift
@@ -0,0 +1,16 @@
+//
+//  Scoreboard.swift
+//  ScoreKeeper
+//
+//  Created by Mikihisa Saito on 2025/12/05.
+//
+
+import Foundation
+
+struct Scoreboard{
+	var players: [Player] = [
+		Player(name: "Elisha", score: 0),
+		Player(name: "Andre", score: 0),
+		Player(name: "Jasmine", score: 0),
+	]
+}
