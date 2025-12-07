af6e9d16d95c61135c808b408087f5d3970edef1
mikio <43379090+mikiolight@users.noreply.github.com>
2025-12-07 17:33:40 +0900

    Section 3: Create a unit test target

---
+++ ScoreKeeperTests/ScoreKeeperTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScoreKeeperTests.swift
+//  ScoreKeeperTests
+//
+//  Created by Mikihisa Saito on 2025/12/07.
+//
+
+import Testing
+
+struct ScoreKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
