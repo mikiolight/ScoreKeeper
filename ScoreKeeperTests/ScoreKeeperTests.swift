ae20d1e911cb1cdcd5cbb999d43576c3c2fa7beb
mikio <43379090+mikiolight@users.noreply.github.com>
2025-12-07 18:27:20 +0900

    Section 4: Test and implement your method

--- ScoreKeeperTests/ScoreKeeperTests.swift
+++ ScoreKeeperTests/ScoreKeeperTests.swift
@@ -6,11 +6,20 @@
 //
 
 import Testing
+@testable import ScoreKeeper
 
 struct ScoreKeeperTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
+	@Test("Reset player scores", arguments: [0,10,20])
+	func resetScores(to newValue: Int) async throws {
+		var scoreboard = Scoreboard(players:[
+			Player(name: "Elisha", score: 0),
+			Player(name: "Andre", score: 5),
+		])
+		await scoreboard.resetScores(to: newValue)
 
+		for player in await scoreboard.players{
+			#expect(player.score == newValue)
+		}
+	}
 }
